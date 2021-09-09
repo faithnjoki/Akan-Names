9558f2f140511c2a82e9db5b0761f59cf3b4f256
faithnjoki <faithnjoki81@gmail.com>
2021-09-09 17:36:38 +0300

    Edit js file

--- file.js
+++ file.js
@@ -1,29 +1,27 @@
  function generateName() {
 var date = document.getElementById("date").value
+var month = document.getElementById("month").value;
 var year = document.getElementById("year").value;
-var birthDay = newDate(year+'/'+month+'/'+date);
+var birthDay = new Date(year+'/'+month+'/'+date);
+
 var dayOfWeek = birthDay.getDay();
+alert (birthDay);
+
 var male = document.getElementById("male").checked;
 var female = document.getElementById("female").checked;
 
 
-//syntax for getting data from selections;
-var month = document.getElementById("month").value;
-//var gender = genders.options[genders.selectedIndex].text
-
-var century = yearOfBirth.slice(0,2);
-//let year = yearOfBirth.slice(2,4);
-
+// var century = yearOfBirth.slice(0,2);
+ //let year = yearOfBirth.slice(2,4);
 
  //formulagiven
- var dayOfBirth = ( ( (century/4) -2*year-1) + ((5*year/4) ) + ((26*(month+1)/10)) + date ) % 7;
- var dayOfBirth = Math.floor(dayOfBirth);
+//  var dayOfBirth = ( ( (century/4) -2*year-1) + ((5*year/4) ) + ((26*(month+1)/10)) + date ) % 7;
+//  var dayOfBirth = Math.floor(dayOfBirth);
  
-
 
   var maleName = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
   var femaleName = ["Akosu", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Amna"];
-  var dayOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Sunday"];
+  var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
  if(date<= 0 || date> 31){
  document.getElementById("Error").innerHTML = "Please enter valid date"
