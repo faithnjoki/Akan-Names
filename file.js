63fcbcb8856740d0c537cf8a2d7e43abe41a82fe
faithnjoki <faithnjoki81@gmail.com>
2021-09-09 15:47:05 +0300

    Edit my project the file js

--- file.js
+++ file.js
@@ -1,35 +1,45 @@
-function generateName(){
-  var date = document.getElementById('date').value;
-  var month = document.getElementById('month').value;
-  var yearOfBirth = document.getElementById('year').value; 
-  var male = document.getElementById("male").checked; 
-  var female=document.getElementById("female").checked;
-   
-  //male
-  var maleName = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+ function generateName() {
+let tarehe = document.getElementById("date").value;
+let mwezi = document.getElementById("month").value;
+let mwaka = document.getElementById("year").value;
+let genders = document.getElementById("gender").value;
+//syntax for getting data from selections
+let gender = genders.options[genders.selectedIndex].text
 
-  //female
-  var femaleName = ["Akosu", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Amna"];
-
-  var dayOfWeek = [ "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday","Sunday"];
-
-  var century = yearOfBirth.slice(0,2);
-  var year = yearOfBirth.slice(2,4);
-  
-  //((CC/4)-2*CC-1) +((5*YY/4) )+ ((26*(MM+1)/10)) + DD )
+let century = yearOfBirth.slice(0,2);
+let year = yearOfBirth.slice(2,4);
 
 
-
-  var dayOfBirth = ( ( (century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) +  dayOfWeek )
+ //formulagiven
+ var dayOfBirth = ( ( (century/4) -2*year-1) + ((5*year/4) ) + ((26*(month+1)/10)) + date ) % 7;
   var dayOfBirth = Math.floor(dayOfBirth);
+ 
 
+if(date<= 0 || date> 31){
+  alert("Invalid day");
+}
+if(month<=0 || month>12){
+  alert ("Invalid month")
+}
+
+  var maleName = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+  var femaleName = ["Akosu", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Amna"];
+  var dayOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Sunday"];
+
+  
 
   if (gender === "Male") {
-       document.getElementById("result").innerHTML = "You were born on " + dayOfWeek[dayOfBirth -1]+ " and your name is " +  maleName[dayOfBirth -1];
+       document.getElementById("result").innerHTML = "You were born on " + dayOfWeek[dayOfBirth - 1]+ " and your name is " +  maleName[dayOfBirth -1];
        }
        else if (gender === "Female") {
-           document.getElementById("result").innerHTML = "You were born on " + dayOfWeek[dayOfBirth -1]+ " and your name is " +  femaleName[dayOfBirth -1];
-       }   
+           document.getElementById("result").innerHTML = "You were born on " + dayOfWeek[dayOfBirth - 1]+ " and your name is " +  femaleName[dayOfBirth -1];
+       }
+    
 }
+
+
+
+
+
 
 
